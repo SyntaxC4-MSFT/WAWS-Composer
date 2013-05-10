522a242e8fa9357172d6ddc84a7792a14759a5db
Cory Fowler <syntaxc4@gmail.com>
2013-05-09 23:07:35 -0700

    added web.config rewrite rules and composer download

--- deploy.cmd
+++ deploy.cmd
@@ -48,6 +48,20 @@ IF NOT DEFINED KUDU_SYNC_CMD (
 )
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Dependencies
+:: ------------
+
+echo Downloading Dependencies
+
+IF NOT DEFINED COMPOSER_PHAR_PATH {
+	SET COMPOSER_PHAR_PATH=%DEPLOYMENT_TARGET%\bin
+	echo Downloading Composer
+	curl -sS -o 'composer.phar' https://getcomposer.org/installer | php -- --install-dir=%COMPOSER_PHAR_PATH%
+	
+	IF !ERRORLEVEL! NEQ 0 goto error
+}
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Deployment
 :: ----------
 
