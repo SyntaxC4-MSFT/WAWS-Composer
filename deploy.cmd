813dbefa93c603eb04d837e40d20c74e02c2d54f
Cory Fowler <syntaxc4@gmail.com>
2013-05-10 15:23:50 -0700

    changing the composer path

--- deploy.cmd
+++ deploy.cmd
@@ -54,23 +54,18 @@ IF NOT DEFINED KUDU_SYNC_CMD (
 echo Downloading Dependencies
 
 IF NOT DEFINED COMPOSER_INSTALL_PATH (
-	SET COMPOSER_INSTALL_PATH=%DEPLOYMENT_TARGET%\web\bin
-	
-	IF NOT DEFINED COMPOSER_PHAR_PATH (
-		SET COMPOSER_PHAR_PATH="%appdata%\composer"
-		mkdir "%appdata%\composer"
-	)
+	SET COMPOSER_INSTALL_PATH=%DEPLOYMENT_SOURCE%\web\bin
 	
 	echo Downloading Composer
-	pushd %COMPOSER_PHAR_PATH%
 	
-	curl -sS -o composer.phar https://getcomposer.org/installer | php -- --install-dir %COMPOSER_INSTALL_PATH%
+	pushd %COMPOSER_INSTALL_PATH%
+	curl -sS -o composer.phar https://getcomposer.org/installer | php
 	popd
 	
 	IF !ERRORLEVEL! NEQ 0 goto error
 )
 
-php %COMPOSER_PHAR_PATH%\composer.phar install
+php %COMPOSER_INSTALL_PATH%\composer.phar install
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Deployment
