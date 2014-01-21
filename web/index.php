c7397bf0b5c7565847b07f334906fbe884c527ed
Cory Fowler <syntaxc4@gmail.com>
2014-01-21 13:53:18 -0800

    added a call to blob storage

---
+++ web/index.php
@@ -0,0 +1,21 @@
+require_once "../bin/vendor/autoload.php";
+
+use WindowsAzure\Common\ServicesBuilder;
+use WindowsAzure\Common\ServiceException;
+
+$blobRestProxy = ServicesBuilder::getInstance()->createBlobService(getEnv('CUSTOMCONNSTR_BlobConnectionString'));
+
+try {
+    // List blobs.
+    $blob_list = $blobRestProxy->listBlobs("training-kit");
+    $blobs = $blob_list->getBlobs();
+
+    foreach($blobs as $blob)
+    {
+        echo $blob->getName().": ".$blob->getUrl()."<br />";
+    }
+} catch(ServiceException $e){
+    $code = $e->getCode();
+    $error_message = $e->getMessage();
+    echo $code.": ".$error_message."<br />";
+}
